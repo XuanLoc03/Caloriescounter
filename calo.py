60a11342a34c0a38a48306f994e23cc82eac9d8e
XuanLoc03 <110718555+XuanLoc03@users.noreply.github.com>
2022-11-02 23:55:41 +0700

    Add files via upload

--- calo.py
+++ calo.py
@@ -1,21 +1,20 @@
-import csv
 import pandas as pd
 import streamlit as st
-#21500000
 
 user_data = [{'id':11511200, 'name':'Chocolate milk, ready to drink, reduced fat', 'weight':10, 'exp_date':'20-10-2022'},
              {'id':21500000, 'name':'Ground beef, raw', 'weight':4, 'exp_date':'24-10-2022'}]
-gram = 0
+gram= 0
 calo = 0
-i = 0
 protein = 0
 carb = 0
 fat = 0
-prd_data = []
-prd_total = []
+i = 0
 foodid = 0
+prd_data = []
+#nhung san pham co trong tu lanh = prd_total
 for food in user_data:
     prd_data.append(food['name'])
+#cac ham
 def calculatecalo(foodcode,i):
     unit_data = pd.read_csv("C:\\Users\\Lenovo\\Desktop\\Portion.csv")
     ingres_data = pd.read_csv("C:\\Users\\Lenovo\\Desktop\\Nutrition.csv")
@@ -26,24 +25,26 @@ def calculatecalo(foodcode,i):
     gram = int(unitgr['weight']) * portion
     nutri = ingres_data.loc[ingres_data['Food code'] == foodcode]
     return gram,nutri
-if 'button_clicked' not in st.session_state:
-    st.session_state.button_clicked = False
-def callback():
-    st.session_state.button_clicked = True
-if st.button('Add',on_click= callback) or st.session_state.button_clicked:
-    options = st.selectbox('Choose product',prd_data,key = i)
-    if options:
-        for food1 in user_data:
-            if food1['name'] == options:
-                foodid = food1['id']
-                break
-        gram,nutri = calculatecalo(foodid,i)
-        calo += int(nutri['Energy']) * (gram / 100)
-        protein += int(nutri['Protein (g)']) * (gram / 100)
-        carb += int(nutri['Carbohydrate (g)']) * (gram / 100)
-        fat += int(nutri['Total Fat (g)']) * (gram / 100)
-        i+=2
-st.write('Total calo:',calo)
-st.write('Total carb:',carb)
-st.write('Total protein:',protein)
-st.write('Total fat:',fat)
+def add(options,i,calo,protein,carb,fat):
+    for food1 in user_data:
+        if food1['name'] == options:
+            foodid = food1['id']
+            break
+    st.write('Product:',options)
+    gram,nutri = calculatecalo(foodid,i)
+    calo += int(nutri['Energy']) * (gram / 100)
+    protein += int(nutri['Protein (g)']) * (gram / 100)
+    carb += int(nutri['Carbohydrate (g)']) * (gram / 100)
+    fat += int(nutri['Total Fat (g)']) * (gram / 100)
+    i+=2
+    return calo,protein,carb,fat,i
+def finish(calo,protein,carb,fat):
+    st.write(f'Total calo: {calo} kcal')
+    st.write(f'Total carb: {carb} g')
+    st.write(f'Total protein: {protein} g')
+    st.write(f'Total fat: {fat} g')
+#cac button
+multichoice = st.multiselect('Pick products',prd_data)
+for options in multichoice:
+    calo,protein,carb,fat,i = add(options,i,calo,protein,carb,fat)
+finish(calo,protein,carb,fat)
